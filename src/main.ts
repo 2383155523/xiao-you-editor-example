0c5baf035c23077151cd9d6f8ab07781a12d96a1
微若蜉蝣 <58352334+2383155523@users.noreply.github.com>
2022-06-11 19:24:46 +0800

    Delete src directory

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import xiaoYouEditor from "xiao-you-editor"
-import router from "./router"
-import defineCustomElementsAll from "./customComponents/index"
-
-createApp(App).use(xiaoYouEditor).use(router).mount("#app")
-defineCustomElementsAll()
